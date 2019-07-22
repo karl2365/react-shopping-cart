df699103f4552bd8a0c36330175e6135cc7ae150
Karl Beksa <karlbeksa@gmail.com>
2019-07-22 16:46:48 -0400

    Remove item from cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -6,17 +6,24 @@ import { CartContext } from '../contexts/CartContext';
 
 
 const ShoppingCart = () => {
-	const { cart } = useContext(CartContext);
+	const { cart, setCart } = useContext(CartContext);
+	console.log(cart)
+
+
 	const getCartTotal = () => {
 		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
+	const removeItem = (id) => {
+	    setCart(cart.filter(item => item.id !== id));
+	}
+
 	return (
 		<div className="shopping-cart">
 			{cart.map(item => (
-				<Item key={item.id} {...item} />
+				<Item key={item.id} {...item} removeItem={removeItem} />
 			))}
 
 			<div className="shopping-cart__checkout">
